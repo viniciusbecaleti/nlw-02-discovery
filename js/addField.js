d8b9369990ebddf694f4f7fbb3a0113d6c5b162e
Vinicius Becaleti <vinicius@becaleti.com>
2023-04-25 18:12:55 -0300

    Estrutura HTMl, CSS e JS finalizada

---
+++ js/addField.js
@@ -0,0 +1,18 @@
+const button = document.querySelector("#add-time")
+
+button.addEventListener("click", () => {
+  const scheduleList = document.querySelector("#schedule-items")
+  const scheduleItem = scheduleList.querySelector(".schedule-item")
+
+  // Clona o .schedule-item
+  const clonedScheduleItem = scheduleItem.cloneNode(true)
+
+  // Limpa os inputs
+  const inputs = clonedScheduleItem.querySelectorAll("input")
+  for (const input of inputs) {
+    input.value = ""
+  }
+
+  // Insere + 1 .schedule-item na lista
+  scheduleList.appendChild(clonedScheduleItem)
+})
